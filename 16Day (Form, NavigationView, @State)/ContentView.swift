988886e25f4339bbf3aa30991fd670b7ebaf90ec
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-15 13:13:36 +0300

    Start Project (Empty)

---
+++ 16Day (Form, NavigationView, @State)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  16Day (Form, NavigationView, @State)
+//
+//  Created by Дмитрий on 15.06.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello!!, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
