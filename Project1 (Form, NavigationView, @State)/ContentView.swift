b9906c3a8d52157dc790111df11474d848c45997
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-16 01:43:10 +0300

    Learn @State and binding $

--- Project1 (Form, NavigationView, @State)/ContentView.swift
+++ Project1 (Form, NavigationView, @State)/ContentView.swift
@@ -9,8 +9,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = 0
+
     var body: some View {
-        Text("Hello!!, World Now!")
+        VStack {
+            Picker("Select your student", selection: $selectedStudent) {
+                ForEach(0 ..< students.count) {
+                    Text(self.students[$0])
+                }
+            }
+            Text("You chose: Student # \(students[selectedStudent])")
+        }
     }
 }
 
