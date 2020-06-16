e6cc92f4f88fb1bdb6f2e953a07a347036017377
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-17 00:11:16 +0300

    Day 17 Complete Learn Different Pickers and calculate in var :Type{return}

--- Project1 (Form, NavigationView, @State)/ContentView.swift
+++ Project1 (Form, NavigationView, @State)/ContentView.swift
@@ -9,17 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = 0
+    @State private var numberOfPeople = 0
+    @State private var checkAmount = ""
+    @State private var tipPercentage = 2
+    let tipPercentages = ["5", "10","15","20","0"]
+    
+    var totalPerPerson: Double{
+        let realNofP = Double(numberOfPeople + 2)
+        let realPr = Double(tipPercentages[tipPercentage])!
+        let realAmount = Double(checkAmount.replacingOccurrences(of: ",", with: ".", options: .literal, range: nil)) ?? 0
+        
+        let tipValue = realAmount / 100 * realPr
+        let grandTotal = realAmount + tipValue
+        
+        return grandTotal / realNofP
+    }
+    
+    
 
     var body: some View {
-        VStack {
-            Picker("Select your student", selection: $selectedStudent) {
-                ForEach(0 ..< students.count) {
-                    Text(self.students[$0])
+        NavigationView {
+            Form{
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
                 }
-            }
-            Text("You chose: Student # \(students[selectedStudent])")
+                    
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("From each Person")){
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }.navigationBarTitle("WeSplit")
+            
         }
     }
 }
