ddb2dfcd85d81f9c2c8ceab0bba6d14ca818d098
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-18 13:13:56 +0300

    Initial Commit

---
+++ 19 Day - Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  19 Day - Challenge
+//
+//  Created by Дмитрий on 18.06.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
