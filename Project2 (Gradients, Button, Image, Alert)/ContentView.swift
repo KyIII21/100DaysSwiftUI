4f6bf754fd1d82c83fd0005533ee66c59cdc07b2
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-19 20:42:47 +0300

    Updated added ViewModifier + Extension
    Go back to project 2 and create a FlagImage() view that renders one flag image using the specific set of modifiers we had.

--- Project2 (Gradients, Button, Image, Alert)/ContentView.swift
+++ Project2 (Gradients, Button, Image, Alert)/ContentView.swift
@@ -8,6 +8,21 @@
 
 import SwiftUI
 
+struct OurImage: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            //.renderingMode(.original)
+            .clipShape(Capsule())
+            .overlay(Capsule().stroke(Color.black, lineWidth: 2))
+            .shadow(color: .black, radius: 2)
+    }
+}
+extension View{
+    func ourImageStyle() -> some View{
+        self.modifier(OurImage())
+    }
+}
+
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -45,9 +60,7 @@ struct ContentView: View {
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 2))
-                            .shadow(color: .black, radius: 2)
+                            .ourImageStyle()
                     }
                     .alert(isPresented: self.$showingScore){        Alert(title: Text(self.scoreTitle), message: self.textForAlert(), dismissButton: .default(Text("Ok")){
                             self.askQuestion()
