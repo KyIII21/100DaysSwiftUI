24b47baa091256ea0319ff10199ffea74ff1fccd
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-20 20:22:02 +0300

    Day 27 Finished
    Cleaning up the user interface

--- Project4-BetterRest/ContentView.swift
+++ Project4-BetterRest/ContentView.swift
@@ -13,7 +13,13 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
-    @State private var wakeUp = Date()
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     func calculateBedtime() {
@@ -42,40 +48,44 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+            Form {
+                Section(header: Text("When do you want to wake up?")){
+                    //Text("When do you want to wake up?")
+                        //.font(.headline)
 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        //.labelsHidden()
+                }
 
-                Text("Desired amount of sleep")
-                    .font(.headline)
+                //Text("Desired amount of sleep")
+                   // .font(.headline)
 
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
+                Section(header: Text("Desired amount of sleep")){
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
                 }
                 
-                Text("Daily coffee intake")
-                    .font(.headline)
-
-                Stepper(value: $coffeeAmount, in: 1...20) {
-                    if coffeeAmount == 1 {
-                        Text("1 cup")
-                    } else {
-                        Text("\(coffeeAmount) cups")
-                    }
+                Section(header: Text("Desired amount of sleep")){
+                   Stepper(value: $coffeeAmount, in: 1...20) {
+                       if coffeeAmount == 1 {
+                           Text("1 cup")
+                       } else {
+                           Text("\(coffeeAmount) cups")
+                       }
+                   }
                 }
-                .navigationBarTitle("BetterRest")
-                .navigationBarItems(trailing:
-                        Button(action: calculateBedtime) {
-                            Text("Calculate")
-                    }
-                    .alert(isPresented: $showingAlert) {
-                        Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-                    }
-                )
+                
             }
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                    Button(action: calculateBedtime) {
+                        Text("Calculate")
+                }
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                }
+            )
         }
     }
 }
