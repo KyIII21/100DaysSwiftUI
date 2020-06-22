4b45c261c44f41dc3615bb988a277fb56c812d7f
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-22 20:37:36 +0300

    Project6 Day 33 Animating gestures

--- Project6-Animations/ContentView.swift
+++ Project6-Animations/ContentView.swift
@@ -9,19 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    let letters = Array("Hello SwiftUI")
     @State private var enabled = false
-    
+    @State private var dragAmount = CGSize.zero
+
     var body: some View {
-        Button("Tap Me") {
-            self.enabled.toggle()
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 15))
+            }
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? Color.blue : Color.red)
-        .animation(.default)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        .gesture(
+            DragGesture()
+                .onChanged { self.dragAmount = $0.translation }
+                .onEnded { _ in
+                    self.dragAmount = .zero
+                    self.enabled.toggle()
+                }
+        )
     }
 }
 
