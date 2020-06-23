dd4779c30fcd772a2f53b30e0fdc4f193ce0b765
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-23 22:25:22 +0300

    Initial Commit

---
+++ Edutainment for Kids/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edutainment for Kids
+//
+//  Created by Дмитрий on 23.06.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
