b29912b45e4b65d5786efc39586eb29ffb4a69cc
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-26 01:03:10 +0300

    Initial Commit

---
+++ Project7 - IExpense ( @ObservedObject, sheet(), onDelete() )/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project7 - IExpense ( @ObservedObject, sheet(), onDelete() )
+//
+//  Created by Дмитрий on 26.06.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
