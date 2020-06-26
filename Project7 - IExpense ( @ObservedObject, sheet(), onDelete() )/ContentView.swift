3fb734efb7cd6556c6f40306b4afdc5d6fe2d801
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-27 02:20:00 +0300

    Archiving Swift objects with Codable ( JSONEncoder and JSONDecoder )

--- Project7 - IExpense ( @ObservedObject, sheet(), onDelete() )/ContentView.swift
+++ Project7 - IExpense ( @ObservedObject, sheet(), onDelete() )/ContentView.swift
@@ -8,13 +8,40 @@
 
 import SwiftUI
 
+struct User: Codable {
+    var firstName: String
+    var lastName: String
+}
+
 struct ContentView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+    @State private var showingUser = false
+    @State private var newUser = User(firstName: "", lastName: "")
 
     var body: some View {
-        Button("Tap count: \(tapCount)") {
-            self.tapCount += 1
-            UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+        VStack{
+            Button("Save User") {
+                let encoder = JSONEncoder()
+
+                if let data = try? encoder.encode(self.user) {
+                    UserDefaults.standard.set(data, forKey: "UserData")
+                }
+            }
+            
+            Button("Print User from Data"){
+                let decoder = JSONDecoder()
+                
+                if let newUser = try? decoder.decode(User.self, from: UserDefaults.standard.data(forKey: "UserData")!){
+                    
+                    self.newUser = newUser
+                    self.showingUser.toggle()
+                }
+                
+            }
+            .sheet(isPresented: $showingUser){
+                Text("\(self.newUser.firstName)")
+            }
+            
         }
     }
 }
