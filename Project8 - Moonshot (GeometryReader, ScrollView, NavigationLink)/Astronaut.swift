295da436e33178dde6266da414d46c6ae2e116ba
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-28 22:15:51 +0300

    Day 40 finished

---
+++ Project8 - Moonshot (GeometryReader, ScrollView, NavigationLink)/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Project8 - Moonshot (GeometryReader, ScrollView, NavigationLink)
+//
+//  Created by Дмитрий on 28.06.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
