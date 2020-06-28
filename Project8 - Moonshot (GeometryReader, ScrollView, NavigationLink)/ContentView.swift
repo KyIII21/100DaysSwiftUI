4dd60cf896e86e20293d6e007911db5d55d5aa15
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-28 14:34:10 +0300

    Day 39 How ScrollView lets us work with scrolling data

--- Project8 - Moonshot (GeometryReader, ScrollView, NavigationLink)/ContentView.swift
+++ Project8 - Moonshot (GeometryReader, ScrollView, NavigationLink)/ContentView.swift
@@ -8,15 +8,41 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            GeometryReader { geo in
-                Image("Kush")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: geo.size.width)
+        HStack{
+            ScrollView(.vertical) {
+                VStack(spacing: 10) {
+                    ForEach(0..<100) {
+                        //Создаются сразу
+                        CustomText("Item \($0)")
+                            .font(.title)
+                    }
+                }
             }
+            ScrollView(.vertical) {
+                VStack(spacing: 10) {
+                    ForEach(0..<100) {
+                        //Создаются по мере прокручивания
+                        Text("Item \($0)")
+                            .font(.title)
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
