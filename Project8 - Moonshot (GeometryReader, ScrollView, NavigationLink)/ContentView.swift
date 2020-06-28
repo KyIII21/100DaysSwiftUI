1d612bcfef715665d2dcb200b8ba24b045f29157
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-28 15:25:24 +0300

    Da 39 Finished Working with hierarchical Codable data

--- Project8 - Moonshot (GeometryReader, ScrollView, NavigationLink)/ContentView.swift
+++ Project8 - Moonshot (GeometryReader, ScrollView, NavigationLink)/ContentView.swift
@@ -8,29 +8,46 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    var text: String
-
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
+struct User: Codable {
+    var name: String
+    var address: Address
+    
+    init(){
+        name = ""
+        address = Address(street: "", city: "")
     }
 }
 
+struct Address: Codable {
+    var street: String
+    var city: String
+}
+
 struct ContentView: View {
+    @State private var newUser = User()
     var body: some View {
-        NavigationView {
-            List(0..<100) { row in
-                NavigationLink(destination: Text("Detail \(row)")) {
-                    Text("Row \(row)")
+        VStack{
+            Button("Decode JSON") {
+                let input = """
+                {
+                    "name": "Taylor Swift",
+                    "address": {
+                        "street": "555, Taylor Swift Avenue",
+                        "city": "Nashville"
+                    }
+                }
+                """
+
+                let data = Data(input.utf8)
+                let decoder = JSONDecoder()
+                if let user = try? decoder.decode(User.self, from: data) {
+                    self.newUser = user
                 }
             }
-            .navigationBarTitle("SwiftUI")
+                .padding()
+            Text("User Address: \(self.newUser.address.street)")
         }
+        .navigationBarTitle("SwiftUI")
     }
 }
 
