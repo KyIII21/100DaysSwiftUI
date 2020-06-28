0f181476b79f27137b6c8984138b7f39b3957eec
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-28 13:34:41 +0300

    Initial Commit

---
+++ Project8 - Moonshot (GeometryReader, ScrollView, NavigationLink)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project8 - Moonshot (GeometryReader, ScrollView, NavigationLink)
+//
+//  Created by Дмитрий on 28.06.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
