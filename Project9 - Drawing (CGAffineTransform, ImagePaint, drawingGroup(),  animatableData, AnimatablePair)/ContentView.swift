5a24a31e17898628e476ddf9797d8cb1fea3ae24
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-06-30 20:20:00 +0300

    Day 43 Adding strokeBorder() support with InsettableShape

--- Project9 - Drawing (CGAffineTransform, ImagePaint, drawingGroup(),  animatableData, AnimatablePair)/ContentView.swift
+++ Project9 - Drawing (CGAffineTransform, ImagePaint, drawingGroup(),  animatableData, AnimatablePair)/ContentView.swift
@@ -8,23 +8,18 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-
-        return path
-    }
-}
-
-struct Arc: Shape {
+struct Arc: InsettableShape {
     var startAngle: Angle
     var endAngle: Angle
     var clockwise: Bool
+    
+    var insetAmount: CGFloat = 0
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
 
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
@@ -32,7 +27,8 @@ struct Arc: Shape {
         let modifiedEnd = endAngle - rotationAdjustment
 
         var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        //path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
 
         return path
     }
@@ -40,17 +36,8 @@ struct Arc: Shape {
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            Triangle()
-                .stroke(Color.red, style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
-                .frame(width: 200, height: 200)
-                .padding()
-            
-            Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
-                .stroke(Color.blue, lineWidth: 10)
-                .frame(width: 300, height: 300)
-        }
-        
+        Circle()
+        .strokeBorder(Color.blue, lineWidth: 80)
     }
 }
 
