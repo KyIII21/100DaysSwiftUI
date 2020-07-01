0ea829542bec54e758cbde422c2adb24d74ffa4b
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-01 12:53:44 +0300

    Day 44 Enabling high-performance Metal rendering with drawingGroup()

--- Project9 - Drawing (CGAffineTransform, ImagePaint, drawingGroup(),  animatableData, AnimatablePair)/ContentView.swift
+++ Project9 - Drawing (CGAffineTransform, ImagePaint, drawingGroup(),  animatableData, AnimatablePair)/ContentView.swift
@@ -8,20 +8,46 @@
 
 import SwiftUI
 
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: CGFloat(value))
+                    // Можно использовать и без drawingGroup()
+                    //.strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup() // Перестало тормозить при прорисовке
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
 
     var body: some View {
         VStack {
-            Text("Hello World")
-            .frame(width: 300, height: 300)
-            .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
-            
-            Capsule()
-            .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 50)
-            .frame(width: 300, height: 200)
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+
+            Slider(value: $colorCycle)
         }
     }
 }
