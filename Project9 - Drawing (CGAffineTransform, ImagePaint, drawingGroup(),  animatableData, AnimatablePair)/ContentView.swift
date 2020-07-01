b18ad190f0efac2ced4755862fe4fb103760fe68
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-01 13:28:14 +0300

    Day 45 Animating simple shapes with animatableData

--- Project9 - Drawing (CGAffineTransform, ImagePaint, drawingGroup(),  animatableData, AnimatablePair)/ContentView.swift
+++ Project9 - Drawing (CGAffineTransform, ImagePaint, drawingGroup(),  animatableData, AnimatablePair)/ContentView.swift
@@ -8,58 +8,39 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+   }
+}
 
 struct ContentView: View {
-    @State private var amount: CGFloat = 0.0
+    @State private var insetAmount: CGFloat = 50
 
     var body: some View {
-        /*
-         ZStack {
-            Image("Example")
-
-            Rectangle()
-                .fill(Color.red)
-                .blendMode(.multiply)
-        }
-        .frame(width: 400, height: 500)
-        .clipped()
-         */
-        
-        VStack {
-            Image("Example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 300, height: 250)
-                .saturation(Double(amount))
-                .blur(radius: (1 - amount) * 20)
-            
-            ZStack {
-                Circle()
-                    .fill(Color(red: 1, green: 0, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-
-                Circle()
-                    .fill(Color(red: 0, green: 1, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-
-                Circle()
-                    .fill(Color(red: 0, green: 0, blue: 1))
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
             }
-            .frame(width: 300, height: 300)
-
-            Slider(value: $amount)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
-        .edgesIgnoringSafeArea(.all)
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
