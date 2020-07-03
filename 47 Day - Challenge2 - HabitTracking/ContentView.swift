e9ab1e6d4da5f6b7c18d562aca45ae3aa589433d
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-04 00:51:24 +0300

    Day 47 Finished
    At the very least, this means there should be a list of all activities they want to track, plus a form to add new activities – a title and description should be enough.
    
    For a bigger challenge, tapping one of the activities should show a detail screen with the description, how many times they have completed it, plus a button incrementing their completion count.
    
    For an even bigger challenge, use Codable and UserDefaults to load and save all your data.

--- 47 Day - Challenge2 - HabitTracking/ContentView.swift
+++ 47 Day - Challenge2 - HabitTracking/ContentView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var listHabbits = Habbits()
+    @State private var showingSheetAddH = false
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            VStack{
+                List{
+                    ForEach(self.listHabbits.arrHabbits){item in
+                        NavigationLink(destination: HabbitView(habbit: item, habbits: self.listHabbits)){
+                            HStack{
+                                Text("\(item.printName())")
+                                Spacer()
+                                Text("\(item.count)")
+                            }
+                        }
+                    }
+                    .onDelete(perform: self.listHabbits.deleteHabbit)
+                }
+                
+            }
+            .navigationBarTitle("HabbitTracking")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {self.showingSheetAddH = true}){
+                Image(systemName: "plus")
+            }
+                .sheet(isPresented: self.$showingSheetAddH){
+                    SheetAddH(listHabbits: self.listHabbits)
+                }
+            )
+        }
+        
     }
 }
 
@@ -19,3 +47,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
---
+++ 47 Day - Challenge2 - HabitTracking/SheetAddH.swift
@@ -0,0 +1,49 @@
+//
+//  SheetAddH.swift
+//  47 Day - Challenge2 - HabitTracking
+//
+//  Created by Дмитрий on 03.07.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct SheetAddH: View {
+    @State private var name = ""
+    @State private var description = ""
+    @ObservedObject var listHabbits: Habbits
+    @Environment(\.presentationMode) var prMode
+    
+    func save(){
+        //self.listHabbits.arrHabbits.append(Habbit(name: self.name, description: self.description))
+        self.listHabbits.add(habbit: Habbit(name: self.name, description: self.description))
+        self.prMode.wrappedValue.dismiss()
+        
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+                Form{
+                    Section(header: Text("Habbit name")){
+                        TextField("Ex. chess..", text: self.$name)
+                    }
+                    Section(header: Text("Description")){
+                        TextField("What will You doing?", text: self.$description)
+                    }
+                }
+                
+            }
+                .navigationBarTitle("Add Habbit")
+                .navigationBarItems(trailing: Button(action: self.save){
+                    Text("Save")
+            })
+        }
+    }
+}
+
+struct SheetAddH_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetAddH(listHabbits: Habbits())
+    }
+}
