f7cca285ce4e5213f0f524e64c4b35a1102915b8
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-05 01:49:43 +0300

    Day 49 Finished Validating and disabling forms

--- Project10 - CupcakeCorner (custom Codable implementations, URLSession, the disabled() modifier)/ContentView.swift
+++ Project10 - CupcakeCorner (custom Codable implementations, URLSession, the disabled() modifier)/ContentView.swift
@@ -8,54 +8,28 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        self.results = decodedResponse.results
-                    }
-
-                    // everything is good, so we can exit
-                    return
-                }
-            }
-
-            // if we're still here it means there was a problem
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
+    @State private var username = ""
+    @State private var email = ""
+    var disableForm: Bool {
+        username.count < 4 || email.count < 5
     }
 
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }
+            .disabled(disableForm)
+
         }
-        .onAppear(perform: loadData)
     }
 }
 
