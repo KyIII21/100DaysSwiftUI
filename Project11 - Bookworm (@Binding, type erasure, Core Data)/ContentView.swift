116eeb9f36fb83eb1e8ea539a71f37a1105f99a9
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-06 19:04:31 +0300

    Initial Commit

---
+++ Project11 - Bookworm (@Binding, type erasure, Core Data)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project11 - Bookworm (@Binding, type erasure, Core Data)
+//
+//  Created by Дмитрий on 06.07.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
