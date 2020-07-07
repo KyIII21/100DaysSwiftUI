6571c7847bba1ff2b634f068b18dbb67f4c8dd46
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-07 19:32:39 +0300

    Day 54 Building a list with @FetchRequest

---
+++ Project11 - Bookworm (@Binding, type erasure, Core Data)/EmojiRatingView.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiRatingView.swift
+//  Project11 - Bookworm (@Binding, type erasure, Core Data)
+//
+//  Created by Дмитрий on 07.07.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("💩")
+        case 2:
+            return Text("🙁")
+        case 3:
+            return Text("😐")
+        case 4:
+            return Text("😊")
+        default:
+            return Text("🤩")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
+
