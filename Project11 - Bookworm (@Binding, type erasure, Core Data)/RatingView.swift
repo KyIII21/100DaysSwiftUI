87de3597837accc4445b37ef95d697e26b14ddfc
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-07 17:56:38 +0300

    Day 54 Adding a custom star rating component

---
+++ Project11 - Bookworm (@Binding, type erasure, Core Data)/RatingView.swift
@@ -0,0 +1,53 @@
+//
+//  RatingView.swift
+//  Project11 - Bookworm (@Binding, type erasure, Core Data)
+//
+//  Created by Дмитрий on 07.07.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+
+    var label = ""
+
+    var maximumRating = 5
+
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
+    }
+    
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+
+            ForEach(1..<maximumRating + 1) { number in
+                self.image(for: number)
+                    .foregroundColor(number > self.rating ? self.offColor : self.onColor)
+                    .onTapGesture {
+                        self.rating = number
+                    }
+            }
+        }
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView(rating: .constant(4))
+    }
+}
