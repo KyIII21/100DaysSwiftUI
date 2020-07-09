0a6e7dd50006b7d1827cd6a91a3dc2fc911fca8a
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-09 12:39:26 +0300

    Commit old project 11 Start new Project 12

---
+++ Project12 - CoreDataProject (\.self, how to save a managed object context only when needed, how to ensure objects are unique) /Project12 - CoreData/Project12 - CoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project12 - CoreData
+//
+//  Created by Дмитрий on 09.07.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
