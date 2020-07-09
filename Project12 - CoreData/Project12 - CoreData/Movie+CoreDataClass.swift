92cdf26f57d286fd4389bdfe0a8d2a4d283374d1
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-09 13:51:49 +0300

    Day 57 Creating NSManagedObject subclasses

---
+++ Project12 - CoreData/Project12 - CoreData/Movie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Movie+CoreDataClass.swift
+//  Project12 - CoreData
+//
+//  Created by Дмитрий on 09.07.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
---
+++ Project12 - CoreData/Project12 - CoreData/Movie+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Movie+CoreDataProperties.swift
+//  Project12 - CoreData
+//
+//  Created by Дмитрий on 09.07.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Movie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var title: String
+    @NSManaged public var director: String
+    @NSManaged public var year: Int16
+
+}
