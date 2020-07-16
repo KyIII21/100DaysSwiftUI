6715f951b3840f9d0199f01c20a94397392a6fd1
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-17 00:14:58 +0300

    Day 64 Using coordinators to manage SwiftUI view controllers

--- Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ContentView.swift
+++ Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ContentView.swift
@@ -12,7 +12,13 @@ import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var image: Image?
+    @State private var inputImage: UIImage?
     @State private var showingImagePicker = false
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
 
     var body: some View {
         VStack {
@@ -24,8 +30,8 @@ struct ContentView: View {
                self.showingImagePicker = true
             }
         }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
         }
     }
 }
--- Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ImagePicker.swift
+++ Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ImagePicker.swift
@@ -9,14 +9,37 @@
 import Foundation
 import SwiftUI
 
+
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
         return picker
     }
     
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
         
+    }
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
     
 }
