b0e0ac9597a99a57e676ac4c99423cbfe9bca649
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-19 00:00:13 +0300

    Day 65 Building our basic UI

--- Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ContentView.swift
+++ Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ContentView.swift
@@ -12,30 +12,48 @@ import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-        //Сохранение фото в библиотеку
-        //UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
-        let imageSaver = ImageSaver()
-        imageSaver.writeToPhotoAlbum(image: inputImage)
-    }
+    @State private var filterIntensity = 0.5
 
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
 
-            Button("Select Image") {
-               self.showingImagePicker = true
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    // select an image
+                }
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: self.$filterIntensity)
+                }.padding(.vertical)
+
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+
+                    Spacer()
+
+                    Button("Save") {
+                        // save the picture
+                    }
+                }
             }
-        }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: self.$inputImage)
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
         }
     }
 }
