7d7fd7442e5d5991b426187bd7ba8342aa4cf1ec
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-19 00:34:16 +0300

    Day 65 Finished Basic image filtering using Core Image

--- Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ContentView.swift
+++ Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ContentView.swift
@@ -15,14 +15,39 @@ struct ContentView: View {
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 
     var body: some View {
-        NavigationView {
+        let intensity = Binding<Double>(
+            get: {
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+        return NavigationView {
             VStack {
                 ZStack {
                     Rectangle()
@@ -44,7 +69,7 @@ struct ContentView: View {
 
                 HStack {
                     Text("Intensity")
-                    Slider(value: self.$filterIntensity)
+                    Slider(value: intensity)
                 }.padding(.vertical)
 
                 HStack {
