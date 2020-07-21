20cdf8388abfc6cefc554c326580a1ca5883289a
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-22 00:25:05 +0300

    Day 66 Customizing our filter using ActionSheet

--- Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ContentView.swift
+++ Project13 - Instafilter (CoreImage, Uikit)/Project13 - Instafilter (CoreImage, Uikit)/ContentView.swift
@@ -15,7 +15,8 @@ struct ContentView: View {
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var showingFilterSheet = false
     let context = CIContext()
     
     func loadImage() {
@@ -26,8 +27,18 @@ struct ContentView: View {
         applyProcessing()
     }
     
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+    }
+    
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
+        //currentFilter.intensity = Float(filterIntensity)
+        //currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
 
         guard let outputImage = currentFilter.outputImage else { return }
 
@@ -74,7 +85,7 @@ struct ContentView: View {
 
                 HStack {
                     Button("Change Filter") {
-                        // change filter
+                        self.showingFilterSheet = true
                     }
 
                     Spacer()
@@ -88,6 +99,21 @@ struct ContentView: View {
             .navigationBarTitle("Instafilter")
             .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
                 ImagePicker(image: self.$inputImage)
+            }
+            .actionSheet(isPresented: $showingFilterSheet) {
+                ActionSheet(title: Text("Select a filter"), buttons: [
+                    .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
+                    .default(Text("Edges")) { self.setFilter(CIFilter.edges()) },
+                    .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur()) },
+                    .default(Text("Pixellate")) { self.setFilter(CIFilter.pixellate()) },
+                    .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone()) },
+                    .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask()) },
+                    .default(Text("Vignette")) { self.setFilter(CIFilter.vignette()) },
+                    .default(Text("Circular Screen")){
+                        self.setFilter(CIFilter.circularScreen())
+                        },
+                    .cancel()
+                ])
             }
         }
     }
