9d004d32d3f90334a6a48eb29dcdee1d89b5d329
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-27 00:20:06 +0300

    Day 70 Advanced MKMapView with SwiftUI

--- Project14 - BucketList/Project14 - BucketList/ContentView.swift
+++ Project14 - BucketList/Project14 - BucketList/ContentView.swift
@@ -7,48 +7,42 @@
 //
 
 import SwiftUI
-import LocalAuthentication
+import MapKit
+
 
 
 struct ContentView: View {
-    @State private var isUnlocked = false
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-
-        // check whether biometric authentication is possible
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            // it's possible, so go ahead and use it
-            let reason = "We need to unlock your data."
-
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                // authentication has now completed
-                DispatchQueue.main.async {
-                    if success {
-                        self.isUnlocked = true
-                    } else {
-                        // there was a problem
-                    }
-                }
-            }
-        } else {
-            // no biometrics
-        }
-    }
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
 
     var body: some View {
-        VStack {
-            if self.isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
             }
         }
-        .onAppear(perform: authenticate)
-        
-        //MapView()
-            //.edgesIgnoringSafeArea(.all)
     }
 }
 
--- Project14 - BucketList/Project14 - BucketList/MapView.swift
+++ Project14 - BucketList/Project14 - BucketList/MapView.swift
@@ -10,54 +10,84 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
-    class Coordinator: NSObject, MKMapViewDelegate {
-        var parent: MapView
+    @Binding var centerCoordinate: CLLocationCoordinate2D
+    var annotations: [MKPointAnnotation]
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
+    }
 
-        init(_ parent: MapView) {
-            self.parent = parent
-        }
-        
-        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
-            print(mapView.centerCoordinate)
-        }
-        
-        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
-            view.canShowCallout = true
-            return view
+    func updateUIView(_ view: MKMapView, context: Context) {
+        if annotations.count != view.annotations.count {
+            view.removeAnnotations(view.annotations)
+            view.addAnnotations(annotations)
         }
     }
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
-//    func makeUIView(context: //UIViewRepresentableContext<MapView>) -> MKMapView {
-//        Context) -> MKMapView{
-//        let mapView = MKMapView()
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            parent.centerCoordinate = mapView.centerCoordinate
+        }
+
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+    }
+}
+
+//Learning
+//struct MapView: UIViewRepresentable {
+//    class Coordinator: NSObject, MKMapViewDelegate {
+//        var parent: MapView
 //
+//        init(_ parent: MapView) {
+//            self.parent = parent
+//        }
+//
+//        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+//            print(mapView.centerCoordinate)
+//        }
+//
+//        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+//            view.canShowCallout = true
+//            return view
+//        }
+//    }
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//
+////    func makeUIView(context: //UIViewRepresentableContext<MapView>) -> MKMapView {
+////        Context) -> MKMapView{
+////        let mapView = MKMapView()
+////
+////        mapView.delegate = context.coordinator
+////
+////        return mapView
+////    }
+//    func makeUIView(context: Context) -> MKMapView {
+//        let mapView = MKMapView()
 //        mapView.delegate = context.coordinator
+//
+//        let annotation = MKPointAnnotation()
+//        annotation.title = "London"
+//        annotation.subtitle = "Capital of England"
+//        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
+//        mapView.addAnnotation(annotation)
 //
 //        return mapView
 //    }
-    func makeUIView(context: Context) -> MKMapView {
-        let mapView = MKMapView()
-        mapView.delegate = context.coordinator
-
-        let annotation = MKPointAnnotation()
-        annotation.title = "London"
-        annotation.subtitle = "Capital of England"
-        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
-        mapView.addAnnotation(annotation)
-
-        return mapView
-    }
-
-    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
-    }
-}
-
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView()
-    }
-}
+//
+//    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
+//    }
+//}
