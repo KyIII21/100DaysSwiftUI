eeb0738eaf02da748938acda5e8fad8cee26f83e
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-07-29 16:19:47 +0300

    Day 73 Finished Our + button is rather hard to tap. Try moving all its modifiers to the image inside the button – what difference does it make, and can you think why? Having a complex if condition in the middle of ContentView isn’t easy to read – can you rewrite it so that the MapView, Circle, and Button are part of their own view? This might take more work than you think! Our app silently fails when errors occur during biometric authentication. Add code to show those errors in an alert, but be careful: you can only add one alert() modifier to each view.

--- Project14 - BucketList/Project14 - BucketList/ContentView.swift
+++ Project14 - BucketList/Project14 - BucketList/ContentView.swift
@@ -7,48 +7,14 @@
 //
 
 import SwiftUI
-import MapKit
 import LocalAuthentication
 
 
 
-struct ContentView: View {
-    @State private var centerCoordinate = CLLocationCoordinate2D()
-    //@State private var locations = [MKPointAnnotation]()
-    @State private var locations = [CodableMKPointAnnotation]()
-    
-    @State private var selectedPlace: MKPointAnnotation?
-    @State private var showingPlaceDetails = false
-    
-    @State private var showingEditScreen = false
-    
+struct ContentView: View {    
     @State private var isUnlocked = false
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    func loadData() {
-        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
-
-        do {
-            let data = try Data(contentsOf: filename)
-            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
-        } catch {
-            print("Unable to load saved data.")
-        }
-    }
-    
-    func saveData() {
-        do {
-            let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
-            let data = try JSONEncoder().encode(self.locations)
-            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
-        } catch {
-            print("Unable to save data.")
-        }
-    }
+    @State private var showingUnlockAlert = false
+    @State private var errorAuthenticateMessage = ""
     
     func authenticate() {
         let context = LAContext()
@@ -63,46 +29,22 @@ struct ContentView: View {
                     if success {
                         self.isUnlocked = true
                     } else {
-                        // error
+                        self.errorAuthenticateMessage = authenticationError?.localizedDescription ?? "Unknown Error in evaluatePolicy"
+                        self.showingUnlockAlert.toggle()
                     }
                 }
             }
         } else {
             // no biometrics
+            self.errorAuthenticateMessage = error?.localizedDescription ?? "Unknown Error in canEvaluatePolicy (No Biometrics)"
+            self.showingUnlockAlert.toggle()
         }
     }
 
     var body: some View {
-        ZStack {
+        ZStack{
             if isUnlocked {
-                MapView(centerCoordinate: $centerCoordinate, annotations: locations, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
-                    .edgesIgnoringSafeArea(.all)
-                Circle()
-                    .fill(Color.blue)
-                    .opacity(0.3)
-                    .frame(width: 32, height: 32)
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            let newLocation = CodableMKPointAnnotation()
-                            newLocation.coordinate = self.centerCoordinate
-                            newLocation.title = "Example location"
-                            self.locations.append(newLocation)
-                            self.selectedPlace = newLocation
-                            self.showingEditScreen = true
-                        }) {
-                            Image(systemName: "plus")
-                        }
-                        .padding()
-                        .background(Color.black.opacity(0.75))
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
-                        .padding(.trailing)
-                    }
-                }
+                UnlockedView()
             } else {
                 Button("Unlock Places") {
                     self.authenticate()
@@ -113,17 +55,11 @@ struct ContentView: View {
                 .clipShape(Capsule())
             }
         }
-        .alert(isPresented: $showingPlaceDetails) {
-            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
-                self.showingEditScreen = true
-            })
+        .alert(isPresented: self.$showingUnlockAlert){
+            Alert(title: Text("Sorry"), message: Text("\(self.errorAuthenticateMessage)"), primaryButton: .default(Text("Ok")), secondaryButton: .default(Text("Repeat again"), action: {
+                self.authenticate()
+            }))
         }
-        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
-            if self.selectedPlace != nil {
-                EditView(placemark: self.selectedPlace!)
-            }
-        }
-        .onAppear(perform: loadData)
     }
 }
 
---
+++ Project14 - BucketList/Project14 - BucketList/UnlockedView.swift
@@ -0,0 +1,96 @@
+//
+//  UnlockedView.swift
+//  Project14 - BucketList
+//
+//  Created by Дмитрий on 29.07.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct UnlockedView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [CodableMKPointAnnotation]()
+    
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetails = false
+    
+    @State private var showingEditScreen = false
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func loadData() {
+        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+
+        do {
+            let data = try Data(contentsOf: filename)
+            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+        } catch {
+            print("Unable to load saved data.")
+        }
+    }
+    
+    func saveData() {
+        do {
+            let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+            let data = try JSONEncoder().encode(self.locations)
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save data.")
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, annotations: locations, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails)
+                    .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = CodableMKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        newLocation.title = "Example location"
+                        self.locations.append(newLocation)
+                        self.selectedPlace = newLocation
+                        self.showingEditScreen = true
+                    }) {
+                        Image(systemName: "plus")
+                            .padding()
+                            .background(Color.black.opacity(0.75))
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .clipShape(Circle())
+                    }
+                    .padding()
+                }
+            }
+        }
+        .alert(isPresented: $showingPlaceDetails) {
+            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
+                self.showingEditScreen = true
+            })
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
+            if self.selectedPlace != nil {
+                EditView(placemark: self.selectedPlace!)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+}
+
+struct UnlockedView_Previews: PreviewProvider {
+    static var previews: some View {
+        UnlockedView()
+    }
+}
