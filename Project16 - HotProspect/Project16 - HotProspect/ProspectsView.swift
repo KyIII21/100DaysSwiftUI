44a5778623fc211d04dd32965267eacc829c43ac
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-08-21 11:17:12 +0300

    Day 84 Saving and loading data with UserDefaults

--- Project16 - HotProspect/Project16 - HotProspect/ProspectsView.swift
+++ Project16 - HotProspect/Project16 - HotProspect/ProspectsView.swift
@@ -51,7 +51,7 @@ struct ProspectsView: View {
            person.name = details[0]
            person.emailAddress = details[1]
 
-           self.prospects.people.append(person)
+           self.prospects.add(person)
        case .failure(let error):
            print("Scanning failed: \(error)")
        }
