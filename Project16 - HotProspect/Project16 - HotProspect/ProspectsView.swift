541595c1c90702e44cbcb64962a741e44341987d
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-08-21 11:59:41 +0300

    Day 84 finished Posting notifications to the lock screen

--- Project16 - HotProspect/Project16 - HotProspect/ProspectsView.swift
+++ Project16 - HotProspect/Project16 - HotProspect/ProspectsView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import CodeScanner
+import UserNotifications
 
 enum FilterType {
     case none, contacted, uncontacted
@@ -57,6 +58,40 @@ struct ProspectsView: View {
        }
     }
     
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+
+//            at 09:00 in the next morning
+//            var dateComponents = DateComponents()
+//            dateComponents.hour = 9
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("D'oh")
+                    }
+                }
+            }
+        }
+    }
+    
     var body: some View {
         NavigationView {
             List {
@@ -70,6 +105,11 @@ struct ProspectsView: View {
                     .contextMenu {
                         Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
                             self.prospects.toggle(prospect)
+                        }
+                        if !prospect.isContacted {
+                            Button("Remind Me") {
+                                self.addNotification(for: prospect)
+                            }
                         }
                     }
                 }
