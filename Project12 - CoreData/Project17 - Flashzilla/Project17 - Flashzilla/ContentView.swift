e36e74751786b19124eb9976791d07df5714a67f
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-08-21 20:27:49 +0300

    Project17 - Flashzilla Day 86 How to use gestures in SwiftUI

---
+++ Project12 - CoreData/Project17 - Flashzilla/Project17 - Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project17 - Flashzilla
+//
+//  Created by Дмитрий on 21.08.2020.
+//  Copyright © 2020 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        UseGesturesVIew()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
