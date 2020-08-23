9b67a75d7e153737fb0c2e75acaf1d088b230456
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-08-23 21:53:53 +0300

    Day 90 Making iPhones vibrate with UINotificationFeedbackGenerator

--- Project17 - Flashzilla/Project17 - Flashzilla/CardView.swift
+++ Project17 - Flashzilla/Project17 - Flashzilla/CardView.swift
@@ -14,6 +14,8 @@ struct CardView: View {
     @State private var offset = CGSize.zero
     
     var removal: (() -> Void)? = nil
+    
+    @State private var feedback = UINotificationFeedbackGenerator()
 
     var body: some View {
         ZStack {
@@ -51,12 +53,18 @@ struct CardView: View {
         }
         .gesture(
             DragGesture()
-                .onChanged { gesture in
-                    self.offset = gesture.translation
+                .onChanged { offset in
+                    self.offset = offset.translation
+                    self.feedback.prepare() // Подготовливаем к вибрации заранее, чтобы не было задержки в 0.5 секунды
                 }
-
                 .onEnded { _ in
                     if abs(self.offset.width) > 100 {
+                        if self.offset.width > 0 {
+                            self.feedback.notificationOccurred(.success)
+                        } else {
+                            self.feedback.notificationOccurred(.error)
+                        }
+
                         self.removal?()
                     } else {
                         self.offset = .zero
