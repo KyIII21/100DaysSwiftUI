e831d59f047a427e9550c8ae2dfb11c49608518e
KyIII21 <66944828+KyIII21@users.noreply.github.com>
2020-08-23 21:17:26 +0300

    Day 89 Counting down with a Timer

--- Project17 - Flashzilla/Project17 - Flashzilla/ContentView.swift
+++ Project17 - Flashzilla/Project17 - Flashzilla/ContentView.swift
@@ -18,6 +18,11 @@ extension View {
 struct ContentView: View {
     @State private var cards = [Card](repeating: Card.example, count: 10)
     
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var isActive = true
+    
     func removeCard(at index: Int) {
         cards.remove(at: index)
     }
@@ -30,6 +35,17 @@ struct ContentView: View {
             .edgesIgnoringSafeArea(.all)
             
             VStack {
+                Text("Time: \(timeRemaining)")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding(.horizontal, 20)
+                .padding(.vertical, 5)
+                .background(
+                    Capsule()
+                        .fill(Color.black)
+                        .opacity(0.75)
+                )
+                
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: self.cards[index]) {
@@ -41,6 +57,18 @@ struct ContentView: View {
                     }
                 }
             }
+        }
+        .onReceive(timer) { time in
+            guard self.isActive else { return }
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            self.isActive = true
         }
     }
 }
